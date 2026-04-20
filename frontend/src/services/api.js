f36846202101c4314b92f784148399ba3701b37a
Vaishnavi Malave <146418791+VaishnaviMalave11@users.noreply.github.com>
2026-04-20 15:46:59 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'https://searfind-backend-api.onrender.com/api',
+  baseURL: 'https://searfind-backend-api.onrender.com/api',
   headers: {
     'Content-Type': 'application/json'
   }
